83d24df5f70b064aff6474df10be62c7e8844822
Samuel Pavon <festovalros@gmail.com>
2017-04-17 00:35:34 -0400

    initial commit

---
+++ conf/users.auth.php
@@ -0,0 +1,23 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+festovalros:$1$x1FnGMFe$BI80ARrO0ouciaQbq.oU0/:Samuel Pavon:festovalros@ararauna.org.ve:admin,user
+perezgabriel13:$1$xXhcPWUs$50ggqJvHb7M7FzeASnuqJ.:Gabriel Perez:gabriel.a.perez@ucv.ve:user
+wilmeragsucv:$1$i55cJbO1$M6cXB1kOVbETxvukXdQA71:Wilmer:wilmer.a.gonzalez@ucv.ve:admin,user
+guachusperquin:$1$vSbMOVlm$GzGBd68b8mbmWVmAzp/V4/:Marcos Garcia:guachusperquin@gmail.com:admin,user
+carlosucv:$1$rsDQfKaZ$2QOoQsMFZfHzzq0uaMZp//:carlos:carlos.correa@ucv.ve:user
+reilyucv:$1$pEO8dJQk$xegZCDy4vy2.RrQEU4lP.1:reily:reily.adames@ucv.ve:user
+magiraucv:$1$mLXVFaGL$VzaFA/bvi67VoDllHe6730:magira:magira.mercado@ucv.ve:user
+dubrascaucv:$1$RGKpDpgt$FrRe3uKzvEcBXSpDktw6N0:dubraska:dubraska.pena@ucv.ve:user
+johathanucv:$1$tQOMHBps$iIiCFkUB7QF.67F2JOdPp/:jonathan:jonathan.chacon@ucv.ve:user
+robertoucv:$1$mMPT1Wwa$wMR5OgBwlCB6qhsL2EW7y/:Roberto González:roberto.gonzalez@ucv.ve:user
+mariangelaucv:$1$FMjjlvbA$S3l5maiTeIpZWR/OdUunl/:Mariangela:mariangela.alvarez@ucv.ve:user,admin
+ivanbadtz:$1$PVX4vG5n$gDJtKl4XiqYJvswbb1aXU.:ivan moreno:ivanmoreno69@hotmail.com:user
